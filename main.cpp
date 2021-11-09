399f52efa9e10fb81a9b818659834de784755062
Maciej Cetera <maciek1128@gmail.com>
2021-11-09 14:29:09 +0100

    drugi commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	float a, b, mz;
+
+	cout << "Podaj a: ", cin >> a;
+    cout << "Podaj b: ", cin >> b;
+
+    if(a == 0 && b == 0){
+        cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
+    } else if(a == 0 && b != 0){
+        cout << "Funkcja nie ma miejsc zerowych" << endl;
+    } else{
+        mz = -b / a;
+        cout << "Miejsce zerowe tej funkcji wynosi: " << mz << endl;
+    }
+
+	return 0;
+}
